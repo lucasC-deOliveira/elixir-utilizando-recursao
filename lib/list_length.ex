ee76853e4fa0da46e09f4121e4366d504d46f496
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-09-04 10:46:51 -0300

    desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum_length(list, 0)
+
+  def sum_length([head | tail], acc) do
+    acc = acc + 1
+    sum_length(tail, acc)
   end
+
+  def sum_length([], acc), do: acc
 end
